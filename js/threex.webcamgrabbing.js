59d6f60606b07a01e0b4710f0752d62e5c4b949a
Kay Chan <wwboy6@gmail.com>
2017-02-21 18:18:29 +0800

    Update camera constraints

--- js/threex.webcamgrabbing.js
+++ js/threex.webcamgrabbing.js
@@ -79,16 +79,15 @@ THREEx.WebcamGrabbing = function(){
 		navigator.mediaDevices.enumerateDevices().then(function(sourceInfos) {
                 // define getUserMedia() constraints
                 var constraints = {
-                        video: { facingMode: { exact: "environment" } },
+                        video: true,
                         audio: false,
                 }
                 // to mirror the video element when it isnt 'environment'
                 // domElement.style.transform   = 'scaleX(-1)'
 				
-				alert("here 1");
+				alert("here 2");
 
                 // it it finds the videoSource 'environment', modify constraints.video
-				/*
                 for (var i = 0; i != sourceInfos.length; ++i) {
                         var sourceInfo = sourceInfos[i];
 						
@@ -104,7 +103,6 @@ THREEx.WebcamGrabbing = function(){
 							}
 						}
                 }
-				*/
 
                 // try to get user media
                 navigator.getUserMedia( constraints, function(stream){
